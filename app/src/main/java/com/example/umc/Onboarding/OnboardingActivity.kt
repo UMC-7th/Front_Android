507ecd931c8b904f70b8cfd28936bd7b74263a5f
Son Juwan <wndhks171@naver.com>
2025-01-31 13:51:15 +0900

    "기본 클릭, 화면 전환 이벤트"

---
+++ app/src/main/java/com/example/umc/Onboarding/OnboardingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.umc.Onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc.Onboarding.DietActivity // DietActivity 패키지 확인
+import com.example.umc.Signin.LoginActivity
+import com.example.umc.databinding.ActivityOnboardingmainBinding
+
+class OnboardingMain : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingmainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingmainBinding.inflate(layoutInflater) // View Binding 설정 확인
+        setContentView(binding.root)
+
+        // 시작 버튼 클릭 시 DietActivity로 이동
+        binding.startButton.setOnClickListener {
+            val intent = Intent(this, DietActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener({
+            val intent =Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        })
+    }
+}
