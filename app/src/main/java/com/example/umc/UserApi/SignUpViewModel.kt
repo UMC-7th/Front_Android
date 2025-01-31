bfaaa394d71756be42bd7c301dd2f7499fce2716
Son Juwan <wndhks171@naver.com>
2025-02-01 05:33:46 +0900

    "request body+viewmodel"

---
+++ app/src/main/java/com/example/umc/UserApi/SignUpViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.umc.UserApi
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    var email: String? = null
+    var password: String? = null
+    var birth: String? = null
+    var name: String? = null
+    var phoneNum: String? = null
+    var purpose: String? = null
+
+    private val userRepository = UserRepository()
+
+    fun sendSignUpRequest(callback: (Boolean, String) -> Unit) {
+        val request = SignUpRequest(
+            email ?: "",
+            password ?: "",
+            birth ?: "",
+            name ?: "",
+            phoneNum ?: "",
+            purpose ?: ""
+        )
+        userRepository.signUp(request, callback)
+    }
+}
