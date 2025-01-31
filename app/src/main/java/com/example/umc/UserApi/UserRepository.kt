3adfd841d16cfdd04891035684cb469ad973ca3b
Son Juwan <wndhks171@naver.com>
2025-02-01 05:34:02 +0900

    "응답 처리 repository"

---
+++ app/src/main/java/com/example/umc/UserApi/UserRepository.kt
@@ -0,0 +1,32 @@
+package com.example.umc.UserApi
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class UserRepository {
+    private val api = RetrofitClient.instance
+
+    // 회원가입 처리
+    fun signUp(request: SignUpRequest, callback: (Boolean, String) -> Unit) {
+        api.signUp(request).enqueue(object : Callback<SignUpResponse> {
+            override fun onResponse(call: Call<SignUpResponse>, response: Response<SignUpResponse>) {
+                if (response.isSuccessful) {
+                    val result = response.body()
+                    callback(result?.success ?: false, result?.message ?: "응답 없음")
+                } else {
+                    callback(false, "서버 오류")
+                }
+            }
+
+            override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
+                callback(false, "네트워크 오류: ${t.message}")
+            }
+        })
+    }
+
+    // 로그인 요청을 Call<LoginResponse>로 반환 (callback 제거)
+    fun login(email: String, password: String): Call<LoginResponse> {
+        return api.login(LoginRequest(email, password))
+    }
+}
