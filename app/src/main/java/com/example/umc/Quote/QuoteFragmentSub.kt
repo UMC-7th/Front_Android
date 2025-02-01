5c7b576d4faa7321003e79f100a81380c1f2a163
Son Juwan <wndhks171@naver.com>
2025-02-01 20:30:50 +0900

    "quote main->sub"

--- app/src/main/java/com/example/umc/Quote/QuoteFragmentSub.kt
+++ app/src/main/java/com/example/umc/Quote/QuoteFragmentSub.kt
@@ -1,11 +1,13 @@
 package com.example.umc.Quote
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.pricefruit.FruitPriceActivity
 import com.example.umc.databinding.FragmentQuoteSubBinding
 
 class QuoteFragmentSub : Fragment() {
@@ -19,6 +21,13 @@ class QuoteFragmentSub : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentQuoteSubBinding.inflate(inflater, container, false)
+
+        // 🍌 banana ID를 가진 ImageView 클릭 시 FruitPriceActivity로 이동
+        binding.banana.setOnClickListener {
+            val intent = Intent(requireContext(), FruitPriceActivity::class.java)
+            startActivity(intent)
+        }
+
         return binding.root
     }
 
