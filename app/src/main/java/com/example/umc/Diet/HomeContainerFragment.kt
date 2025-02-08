b2e144ed603767e9afbcbd9ef2a32a4f3ec44600
MINJU <146050842+MINJU-62@users.noreply.github.com>
2025-02-08 18:16:41 +0900

    [Fiz/#67] SubFragment 수정

--- app/src/main/java/com/example/umc/Diet/HomeContainerFragment.kt
+++ app/src/main/java/com/example/umc/Diet/HomeContainerFragment.kt
@@ -13,6 +13,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.example.umc.Main.MainActivity
 import com.example.umc.Mypage.MyFragment
 import com.example.umc.R
 import com.example.umc.databinding.FragmentHomeContainerBinding
@@ -96,5 +97,10 @@ class HomeContainerFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as? MainActivity)?.hideTitle()
     }
 }
