6c71fb8fbb523085b9258ce136817a5553203703
Kyu hyunSung <dmp100@naver.com>
2025-02-14 04:56:35 +0900

    [FIX/#55] Substatus FIX

--- app/src/main/java/com/example/umc/Subscribe/SubscriptionItem.kt
+++ app/src/main/java/com/example/umc/Subscribe/SubscriptionItem.kt
@@ -2,5 +2,7 @@ package com.example.umc.Subscribe
 
 data class SubscriptionItem(
     val title: String,
-    val description: String
+    val description: String,
+    val item1: String,
+    val item2: String
 )
