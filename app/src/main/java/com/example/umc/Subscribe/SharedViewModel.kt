8a3c46f50d54f2e3e730661e0b27bd20b98870e7
Kyu hyunSung <dmp100@naver.com>
2025-02-21 06:39:46 +0900

    .

--- app/src/main/java/com/example/umc/Subscribe/SharedViewModel.kt
+++ app/src/main/java/com/example/umc/Subscribe/SharedViewModel.kt
@@ -10,10 +10,17 @@ class SubscribeViewModel : ViewModel() {
     private val _selectedDiets = MutableLiveData<List<DietItem>>()
     val selectedDiets: LiveData<List<DietItem>> = _selectedDiets
 
+    // 주문 상세 정보를 저장할 LiveData 추가
+    private val _orderDetails = MutableLiveData<OrderDetails>()
+    val orderDetails: LiveData<OrderDetails> = _orderDetails
+
     fun setSelectedDiets(diets: List<DietItem>) {
         _selectedDiets.value = diets.filter { it.isChecked }
     }
-
+    // 주문 상세 정보를 설정하는 함수
+    fun setOrderDetails(details: OrderDetails) {
+        _orderDetails.value = details
+    }
     fun convertToCartItems(selectedDiets: List<DietItem>): List<CartItem> {
         return selectedDiets.map { diet ->
             CartItem(
@@ -26,3 +33,13 @@ class SubscribeViewModel : ViewModel() {
         }
     }
 }
+
+// 주문 상세 정보를 담는 데이터 클래스 생성
+data class OrderDetails(
+    val orderDate: String,
+    val recipientName: String,
+    val address: String,
+    val phoneNumber: String,
+    val deliveryMemo: String,
+    val orderItems: List<OrderMenuItem>
+)
